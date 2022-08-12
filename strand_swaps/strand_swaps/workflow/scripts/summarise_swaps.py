9874343ff1288d9aabd5e9238b61124264db2f96
Michael Roach <beardymcjohnface@gmail.com>
2022-08-12 16:56:53 +0930

    fix

---
+++ strand_swaps/strand_swaps/workflow/scripts/summarise_swaps.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import re
+current = {'strand':0,'frame':0,'ovlap':0}
+counts = {'strand':[],'frame':[],'ovlap':[]}
+prv = {'start':None,'stop':None,'frame':None,'strand':None}
+with open(snakemake.input[0], 'r') as f:
+    for line in f:
+        line = re.sub('<|>', '', line)
+        l = line.split()
+        coords = l.split('..')
+        frame = str(coords[0] % 3) + l[1]
+        if prv['start'] is None:
+            prv['start'] = coords[0]
+            prv['stop'] = coords[1]
+            prv['strand'] = l[1]
+            prv['frame'] = frame
+            continue
+        if l[1] == prv['strand']:
+            current['strand'] += 1
+            if frame == prv['frame']:
+                current['frame'] += 1
+            else:
+                counts['frame'].append(current['frame'])
+                current['frame'] = 0
+        else:
+            counts['strand'].append(current['strand'])
+            counts['frame'].append(current['frame'])
+            current['strand'] = 0
+            current['frame'] = 0
+        if coords[0] < prv['stop']:
+            current['ovlap'] += 1
+        else:
+            counts['ovlap'].append(current['ovlap'])
+        prv['start'] = coords[0]
+        prv['stop'] = coords[1]
+        prv['strand'] = l[1]
+        prv['frame'] = frame
+st = open(snakemake.output.strnd,'w')
+fr = open(snakemake.output.frame,'w')
+ov = open(snakemake.output.ovlps,'w')
+st.write('\n'.join(counts['strand']))
+fr.write('\n'.join(counts['frame']))
+ov.write('\n'.join(counts['ovlap']))
+st.close()
+fr.close()
+ov.close()
