e32f171ebac3b25e9f85d2cf093b7a609d4bc773
linsalrob <raedwards@gmail.com>
2022-08-13 15:11:33 +0930

    end of Friday commit

--- PhicoreModules/__init__.py
+++ PhicoreModules/__init__.py
@@ -4,9 +4,11 @@ the modules
 
 from .read_genbank import parse_genbank
 from .stats import mean, median, mode, stdev
+from .visualize import visualise
 
 
 __all__ = [
     'parse_genbank', 'mean', 'median', 'mode', 'stdev',
+    'visualise'
 ]
 
