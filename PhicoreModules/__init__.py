41204a75684328e4407b452c5407058ea55442f7
gbouras13 <george.bouras@adelaide.edu.au>
2022-08-18 20:09:59 -0700

    rolling average

--- PhicoreModules/__init__.py
+++ PhicoreModules/__init__.py
@@ -3,7 +3,7 @@ the modules
 """
 
 from .read_genbank import parse_genbank
-from .get_features import get_features_of_type, get_gc_content, get_features_lengths, get_coding_density, get_distribution_of_stops, get_mean_cds_length_rec_window, get_rolling_gc, get_rolling_mean_cds
+from .get_features import get_features_of_type, get_rolling_deltas,get_rolling_count_cds, get_cds_count_length_rec_window, get_gc_content, get_features_lengths, get_coding_density, get_distribution_of_stops, get_mean_cds_length_rec_window, get_rolling_gc,  get_rolling_mean_cds
 from .stats import mean, median, mode, stdev
 from .visualize import visualise
 from .write import write_df_to_artemis
@@ -11,7 +11,7 @@ from .write import write_df_to_artemis
 
 __all__ = [
     'parse_genbank', 'mean', 'median', 'mode', 'stdev',
-    'get_features_of_type', 'get_gc_content', 'get_features_lengths', 'get_coding_density', 'get_distribution_of_stops', 'get_mean_cds_length_rec_window', 'get_rolling_gc', 'get_rolling_mean_cds'
+    'get_features_of_type', 'get_gc_content', 'get_features_lengths', 'get_coding_density', 'get_distribution_of_stops', 'get_mean_cds_length_rec_window', 'get_rolling_gc', 'get_rolling_mean_cds', 'get_rolling_mean_cds_delta',
     'visualise',
     'write_df_to_artemis'
 ]
